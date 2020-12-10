07676745cb1ebbfb5afccec4f368255ee1a1c0ea
AIdan19 <aydenfakirmohamed@gmail.com>
2020-12-10 08:56:10 +0000

    prod + DLC id

--- sql/seed.sql
+++ sql/seed.sql
@@ -4,26 +4,26 @@
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Mortal Kombat 11', 'De gloednieuwe aanpasbare personagevariaties bieden een ongekende controle over je krijgers om er je eigen draai aan te geven. 
+insert into products (name, description, code, price, product_id) values ('Mortal Kombat 11', 'De gloednieuwe aanpasbare personagevariaties bieden een ongekende controle over je krijgers om er je eigen draai aan te geven. 
 
 De nieuwe grafische engine toont elk bottenverpulverend moment in zoveel detail dat je het haast kunt voelen. De toonaangevende filmische verhaalmodus van Mortal Kombat bevat bovendien een lijst met nieuwe en bekende Klassic Fighters en bouwt voort op de meer dan 25 jaar oude epische saga.', 'MK11', 49.99, 1);
 
-insert into products (name, description, code, price) values ('GTA V', 'Een jonge hustler, een gepensioneerde bankrover en een angstaanjagende psychopaat moeten het opnemen tegen de meest gestoorde elementen uit de onderwereld, de Amerikaanse overheid en de entertainment-industrie.
+insert into products (name, description, code, price, product_id) values ('GTA V', 'Een jonge hustler, een gepensioneerde bankrover en een angstaanjagende psychopaat moeten het opnemen tegen de meest gestoorde elementen uit de onderwereld, de Amerikaanse overheid en de entertainment-industrie.
 
 Samen moeten ze een reeks gevaarlijke overvallen uitvoeren en zien te overleven in een meedogenloze stad waarin ze niemand kunnen vertrouwen, en elkaar nog het minst.', 'GTAV', 14,99, 2);
 
-insert into products (name, description, code, price) values ('Geometry Dash', 'Jump and fly your way through danger in this rhythm-based action platformer!', 'GD', 3.99, 3);
+insert into products (name, description, code, price, product_id) values ('Geometry Dash', 'Jump and fly your way through danger in this rhythm-based action platformer!', 'GD', 3.99, 3);
 
-insert into products (name, description, code, price) values ('FINAL FANTASY XV WINDOWS EDITION','In an action-packed battle system, channel the power of your ancestors to warp effortlessly through the air in thrilling combat,
+insert into products (name, description, code, price, product_id) values ('FINAL FANTASY XV WINDOWS EDITION','In an action-packed battle system, channel the power of your ancestors to warp effortlessly through the air in thrilling combat,
 and together with your comrades, master the skills of weaponry, magic and team-based attacks.', 'FF15', 34,99, 4);
 
-insert into products (name, description, code, price) values ('DOOM Eternal Standard Edition', 'Het helleleger heeft de aarde bereikt. Word de Slayer in een epische solocampagne, waarin je demonen afslacht in verschillende dimensies om de totale vernietiging van de mensheid te stoppen. De enige die het helleger vrezen... ben jij.', 'DET', 59.99, 5);
+insert into products (name, description, code, price, product_id) values ('DOOM Eternal Standard Edition', 'Het helleleger heeft de aarde bereikt. Word de Slayer in een epische solocampagne, waarin je demonen afslacht in verschillende dimensies om de totale vernietiging van de mensheid te stoppen. De enige die het helleger vrezen... ben jij.', 'DET', 59.99, 5);
 
-insert into DLC (name, description, code, price) values ('MK11 AFTERMATH', 'Ervaar Mortal Kombats allereerste grote uitbreiding. Aftermath bevat een gloednieuw cinematisch verhaal rondom vertrouwen en bedrog. Vuurgod Liu Kang, de nieuwe tijdbewaarder en beschermer van Earthrealm, wil de toekomst die hij voor zich ziet zeker stellen.
+insert into DLC (name, description, code, price, DLC_id) values ('MK11 AFTERMATH', 'Ervaar Mortal Kombats allereerste grote uitbreiding. Aftermath bevat een gloednieuw cinematisch verhaal rondom vertrouwen en bedrog. Vuurgod Liu Kang, de nieuwe tijdbewaarder en beschermer van Earthrealm, wil de toekomst die hij voor zich ziet zeker stellen.
 
 Om dat te doen moet hij de hulp inschakelen van enkele onwaarschijnlijke bondgenoten en bekende vijanden. Spelers moeten beslissen wie ze kunnen vertrouwen en wie ze moeten verslaan terwijl alles op het spel staat. Smeed een nieuwe geschiedenis.', 'MK11AFT', 39.99, 1);
 
-insert into DLC (name, description, code, price) values ('GTA V Premium Online Edition', 'Grand Theft Auto V, Ontvang meer dan GTA $10.000.000 aan w, Start jouw criminele imperium, Ontvang een verzameling voertuigen, Inclusief krachtige wapens, Ontvang GTA $1.000.000 extra cash', 'GTAVPRE', 34.99, 2);
+insert into DLC (name, description, code, price, DLC_id) values ('GTA V Premium Online Edition', 'Grand Theft Auto V, Ontvang meer dan GTA $10.000.000 aan w, Start jouw criminele imperium, Ontvang een verzameling voertuigen, Inclusief krachtige wapens, Ontvang GTA $1.000.000 extra cash', 'GTAVPRE', 34.99, 2);
 
-insert into DLC (name, description, code, price) values ('FINAL FANTASY XV EPISODE ARDYN','For the first time, players take control of the greatest foe of Noctis in this brand-new episode of FINAL FANTASY XV! Delve into the dark tale of scorned saviour Ardyn Lucis Caelum and unravel the secrets surrounding his mysterious past.', 'FF15NOC', 4,99, 4);
+insert into DLC (name, description, code, price, DLC_id) values ('FINAL FANTASY XV EPISODE ARDYN','For the first time, players take control of the greatest foe of Noctis in this brand-new episode of FINAL FANTASY XV! Delve into the dark tale of scorned saviour Ardyn Lucis Caelum and unravel the secrets surrounding his mysterious past.', 'FF15NOC', 4,99, 4);
 

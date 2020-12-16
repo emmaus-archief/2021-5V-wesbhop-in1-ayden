4833d79554846bb77dfe67d18d61427d8a22ce33
AIdan19 <aydenfakirmohamed@gmail.com>
2020-12-16 11:57:24 +0000

    products fix

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -18,9 +18,18 @@ CREATE TABLE DLC (
   DLC_id INTEGER
 );
 
+/* tabel om aan tegeven welke content bij welk spel gekocht kan worden, n:m-relatie */
 DROP TABLE IF EXISTS rec;
 CREATE TABLE rec (
   ID SERIAL PRIMARY KEY,
   product_id INTEGER,
   DLC_id INTEGER
+);
+
+/* tabel om aan te geven welke spellen(kooptookdit) spelers die een spel spelen (wieditkoopt) ook leuk vinden */
+DROP TABLE IF EXISTS ookleuk;
+CREATE TABLE ookleuk (
+  ID SERIAL PRIMARY KEY,
+  wieditkoopt_id INTEGER,
+  kooptookdit_id2 INTEGER
 );
--- sql/seed.sql
+++ sql/seed.sql
@@ -10,12 +10,12 @@ De nieuwe grafische engine toont elk bottenverpulverend moment in zoveel detail 
 
 insert into products (name, description, code, price, product_id) values ('GTA V', 'Een jonge hustler, een gepensioneerde bankrover en een angstaanjagende psychopaat moeten het opnemen tegen de meest gestoorde elementen uit de onderwereld, de Amerikaanse overheid en de entertainment-industrie.
 
-Samen moeten ze een reeks gevaarlijke overvallen uitvoeren en zien te overleven in een meedogenloze stad waarin ze niemand kunnen vertrouwen, en elkaar nog het minst.', 'GTAV', 14,99, 2);
+Samen moeten ze een reeks gevaarlijke overvallen uitvoeren en zien te overleven in een meedogenloze stad waarin ze niemand kunnen vertrouwen, en elkaar nog het minst.', 'GTAV', 14.99, 2);
 
 insert into products (name, description, code, price, product_id) values ('Geometry Dash', 'Jump and fly your way through danger in this rhythm-based action platformer!', 'GD', 3.99, 3);
 
 insert into products (name, description, code, price, product_id) values ('FINAL FANTASY XV WINDOWS EDITION','In an action-packed battle system, channel the power of your ancestors to warp effortlessly through the air in thrilling combat,
-and together with your comrades, master the skills of weaponry, magic and team-based attacks.', 'FF15', 34,99, 4);
+and together with your comrades, master the skills of weaponry, magic and team-based attacks.', 'FF15', 34.99, 4);
 
 insert into products (name, description, code, price, product_id) values ('DOOM Eternal Standard Edition', 'Het helleleger heeft de aarde bereikt. Word de Slayer in een epische solocampagne, waarin je demonen afslacht in verschillende dimensies om de totale vernietiging van de mensheid te stoppen. De enige die het helleger vrezen... ben jij.', 'DET', 59.99, 5);
 
@@ -25,5 +25,5 @@ Om dat te doen moet hij de hulp inschakelen van enkele onwaarschijnlijke bondgen
 
 insert into DLC (name, description, code, price, DLC_id) values ('GTA V Premium Online Edition', 'Grand Theft Auto V, Ontvang meer dan GTA $10.000.000 aan w, Start jouw criminele imperium, Ontvang een verzameling voertuigen, Inclusief krachtige wapens, Ontvang GTA $1.000.000 extra cash', 'GTAVPRE', 34.99, 2);
 
-insert into DLC (name, description, code, price, DLC_id) values ('FINAL FANTASY XV EPISODE ARDYN','For the first time, players take control of the greatest foe of Noctis in this brand-new episode of FINAL FANTASY XV! Delve into the dark tale of scorned saviour Ardyn Lucis Caelum and unravel the secrets surrounding his mysterious past.', 'FF15NOC', 4,99, 4);
+insert into DLC (name, description, code, price, DLC_id) values ('FINAL FANTASY XV EPISODE ARDYN','For the first time, players take control of the greatest foe of Noctis in this brand-new episode of FINAL FANTASY XV! Delve into the dark tale of scorned saviour Ardyn Lucis Caelum and unravel the secrets surrounding his mysterious past.', 'FF15NOC', 4.99, 4);
 
